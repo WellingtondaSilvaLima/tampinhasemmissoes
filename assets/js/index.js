42bd6318d3085a78a0a5a9288f3b908bd763fdfc
Wellington Lima <wellingtonlima.jt@gmail.com>
2025-07-22 15:01:44 -0300

    Seção concluída: Por que juntar tampinhas

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,13 @@ menuBtn.addEventListener('click', () => {
   navList.classList.toggle('open');
   menuBtn.classList.toggle('open');
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('.fade-on-scroll').forEach(el => observer.observe(el));
